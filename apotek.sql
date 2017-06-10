34092c8b6bbaff979ae37b8ef46db99e44ed9a3e
Fanny Hasbi <fnyhsbi@gmail.com>
2017-06-11 04:37:35 +0700

    Update database structure and its datas.

--- apotek.sql
+++ apotek.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2017 at 12:37 AM
+-- Generation Time: Jun 10, 2017 at 11:32 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
@@ -19,6 +19,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `apotek`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(3) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `username`, `password`, `nama`) VALUES
+(1, 'fannyhasbi', '12345', 'Fanny Hasbi');
 
 -- --------------------------------------------------------
 
@@ -45,7 +65,17 @@ INSERT INTO `detail_pemesanan` (`id`, `kode_pesan`, `kode_obat`, `jumlah`) VALUE
 (8, '2GH3B48', 'C0001', 13),
 (9, 'FAVEJLI', 'B0002', 30),
 (10, 'FAVEJLI', 'C0001', 25),
-(11, 'FAVEJLI', 'A0004', 30);
+(11, 'FAVEJLI', 'A0004', 30),
+(12, 'Q6X46CZ', 'A0001', 15),
+(13, 'Q6X46CZ', 'A0003', 20),
+(14, 'Q6X46CZ', 'C0001', 10),
+(15, 'UBEV4VC', 'A0001', 200),
+(16, 'UBEV4VC', 'A0002', 10),
+(18, 'S244QXZ', 'D0001', 20),
+(19, 'S244QXZ', 'A0004', 10),
+(21, 'HACW9GN', 'D0001', 5),
+(22, 'HACW9GN', 'A0001', 20),
+(24, 'SWWY31J', 'C0001', 10);
 
 -- --------------------------------------------------------
 
@@ -59,6 +89,14 @@ CREATE TABLE `keranjang` (
   `jumlah` int(3) NOT NULL,
   `id_session` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `keranjang`
+--
+
+INSERT INTO `keranjang` (`id`, `kode_obat`, `jumlah`, `id_session`) VALUES
+(21, 'A0001', 10, 'aysho3ajxv6ounnldf3k9n47sr68sxlspz5aqwkx'),
+(22, 'A0002', 20, 'aysho3ajxv6ounnldf3k9n47sr68sxlspz5aqwkx');
 
 -- --------------------------------------------------------
 
@@ -80,13 +118,14 @@ CREATE TABLE `obat` (
 --
 
 INSERT INTO `obat` (`kode_obat`, `nama`, `bentuk`, `konsumen`, `manfaat`, `harga`) VALUES
-('A0001', 'Acarbose', 'Tablet', 'Dewasa', 'Mengontrol kadar gula dalam darah', 5000),
+('A0001', 'Acarbose', 'Tablet, Kapsul', 'Dewasa', 'Mengontrol kadar gula dalam darah', 5000),
 ('A0002', 'Albumin', 'Obat infus', 'Dewasa', 'Menangani defisiensi albumin', 10000),
 ('A0003', 'Amfetamin', 'Tablet, kapsul, dan puyer', 'Dewasa dan anak-anak', 'Menangani ADHD, mengobati narkolepsi, menurunkan berat badan', 12500),
 ('A0004', 'Atenolol', 'Tablet', 'Dewasa', 'Mengobati angina, gangguan detak jantung, dan hipertensi. Menjaga kesehatan jantung Menjaga kesehatan jantu setelah serangan jantung', 25000),
 ('B0001', 'Bacitracin', 'Suntik', 'Dewasa dan anak-anak', 'Mencegah infeksi bakteri pada luka ringan di kulit', 15000),
 ('B0002', 'Baclofen', 'Tablet', 'Dewasa dan anak-anak', 'Meredakan kejang otot', 32000),
-('C0001', 'Captopril', 'Tablet', 'Dewasa', 'Menangani hipertensi, mencegah komplikasi setelah serangan jantung', 52000);
+('C0001', 'Captopril', 'Tablet', 'Dewasa', 'Menangani hipertensi, mencegah komplikasi setelah serangan jantung', 52000),
+('D0001', 'Diazepam', 'Tablet, Obat cair, Suntikan', 'Dewasa dan anak-anak', 'Mengatasi insomnia, serangan kecemasan, melemaskan otot kejang', 12000);
 
 -- --------------------------------------------------------
 
@@ -104,9 +143,14 @@ CREATE TABLE `pembeli` (
 --
 
 INSERT INTO `pembeli` (`id`, `nama`) VALUES
+('21120116120002', 'Kemal Yusron Hasibuan'),
+('21120116130037', 'Ali Sajidin'),
+('21120116130065', 'Adam Maulidani'),
 ('21120116140068', 'Fanny Hasbi'),
 ('21120116140069', 'Fajar Nahari'),
-('21120116140070', 'Azizah Kamalia');
+('21120116140070', 'Azizah Kamalia'),
+('21120116140077', 'Kelvin John'),
+('21120116140078', 'Jeremy Kharisma');
 
 -- --------------------------------------------------------
 
@@ -130,11 +174,22 @@ CREATE TABLE `pemesanan` (
 INSERT INTO `pemesanan` (`kode_pesan`, `id_pemesan`, `harga`, `tanggal`, `status`, `konfirmasi`) VALUES
 ('2GH3B48', '21120116140070', 1136000, '2017-06-09', 'L', '2017-06-09'),
 ('8X60SSU', '21120116140068', 205000, '2017-06-09', 'L', '2017-06-09'),
-('FAVEJLI', '21120116140069', 3010000, '2017-06-09', 'L', '2017-06-09');
+('FAVEJLI', '21120116140069', 3010000, '2017-06-09', 'L', '2017-06-09'),
+('HACW9GN', '21120116140078', 160000, '2017-06-10', 'B', NULL),
+('Q6X46CZ', '21120116120002', 845000, '2017-06-10', 'L', '2017-06-10'),
+('S244QXZ', '21120116140077', 490000, '2017-06-10', 'B', NULL),
+('SWWY31J', '21120116130065', 520000, '2017-06-10', 'B', NULL),
+('UBEV4VC', '21120116130037', 1100000, '2017-06-10', 'B', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
 
 --
 -- Indexes for table `detail_pemesanan`
@@ -174,15 +229,20 @@ ALTER TABLE `pemesanan`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
 -- AUTO_INCREMENT for table `detail_pemesanan`
 --
 ALTER TABLE `detail_pemesanan`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 --
 -- AUTO_INCREMENT for table `keranjang`
 --
 ALTER TABLE `keranjang`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 --
 -- Constraints for dumped tables
 --
